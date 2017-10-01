7721ff2c896fb4a03eeaab779fcc6d52dbd59a34
adexe <alifianadexe@gmail.com>
2017-10-01 13:42:09 +0700

    ver 1.3.1 (Fix Bug Server)

--- Olshop_App/ConnectionClass.vb
+++ Olshop_App/ConnectionClass.vb
@@ -1,7 +1,8 @@
 ﻿Imports System.Configuration
 Module ConnectionClass
+    Dim server As String = "1.1.1.248"
     Public Function generateString() As String
-        Dim connString As String = "Data Source=1.1.1.248,1433;Network Library=DBMSSOCN;Initial Catalog=db_olshop;User id=sa;password=adexe123"
+        Dim connString As String = "Data Source=" + server + ",1433;Network Library=DBMSSOCN;Initial Catalog=db_olshop;User id=sa;password=adexe123"
         Try
             Dim conn As New SqlClient.SqlConnection
             conn.ConnectionString = connString
@@ -11,6 +12,8 @@ Module ConnectionClass
             End If
         Catch ex As Exception
             MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            server = InputBox("Alamat IP Server yang dituju : ", "Server", "127.0.0.1")
+            generateString()
         End Try
         Return connString
     End Function
